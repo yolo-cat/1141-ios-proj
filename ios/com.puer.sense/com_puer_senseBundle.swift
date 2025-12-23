129ca1db36c50099a0fdda24609a667a16077eff
Joseph Tao <josttw@gmail.com>
2025-12-23 12:55:09 +0800

    stage2/ios 整合開發: Introduce new iOS widget extension including standard, control, and live activity widgets.

---
+++ ios/com.puer.sense/com_puer_senseBundle.swift
@@ -0,0 +1,18 @@
+//
+//  com_puer_senseBundle.swift
+//  com.puer.sense
+//
+//  Created by Joseph-M2 on 2025/12/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct com_puer_senseBundle: WidgetBundle {
+    var body: some Widget {
+        com_puer_sense()
+        com_puer_senseControl()
+        com_puer_senseLiveActivity()
+    }
+}
